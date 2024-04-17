46a0a23554981278fd8ff601b1d5fbf631353039
Linhh00008 <dinhphuonglinh_t65@hus.edu.vn>
2024-04-17 11:46:57 +0700

    1

---
+++ src/main/java/vn/com/vhc/amazinglunch/AmazingLunchApplication.java
@@ -0,0 +1,13 @@
+package vn.com.vhc.amazinglunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazingLunchApplication.class, args);
+	}
+
+}
